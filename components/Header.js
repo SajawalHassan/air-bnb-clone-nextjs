9a57dd626b36c6b340c7f59969e752010ef93b62
SajawalHassan <scientificninja1f12@gmail.com>
2021-08-25 20:12:46 +0500

    Completed the Home Page

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import {
 
 function Header() {
   return (
-    <header className="sticky top-0 z-500 grid grid-cols-3 bg-white shadow-lg p-5 md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-lg p-5 md:px-10">
       {/* Left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto p-6">
         <Image
