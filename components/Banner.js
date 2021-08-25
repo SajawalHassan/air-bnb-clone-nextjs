91d8f70f5704e2716550adc05b0f402df209570c
SajawalHassan <scientificninja1f12@gmail.com>
2021-08-25 21:18:53 +0500

    Updated some things here and there

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[400px] sm:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px]">
+    <div className="relative h-[500px] sm:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px]">
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
@@ -10,10 +10,8 @@ function Banner() {
       />
 
       <div className="absolute top-1/2 w-full text-center">
-        <p className="sm:text-lg">Not sure where to go? Perfect!</p>
-
         <button className="text-purple-500 bg-white px-10 py-4 shadow-lg rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
-          I'm flexible
+          S/o Dr.Athar Kharal <br /> Sajawal Hassan <br /> Class 5
         </button>
       </div>
     </div>
